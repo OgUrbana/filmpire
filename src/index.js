aa96d7524f4f4c32a1833a7ecb46a144d59505ab
OgUrbana <urbanajoker@gmail.com>
2023-06-28 18:51:00 -0400

    file & folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App';
 
-import App from './components/App'
+ReactDom.render(
+    <BrowserRouter>
 
-ReactDom.render(<App />, document.getElementById('root'));
+        <App/> 
+    </BrowserRouter>, 
+    document.getElementById('root'));
